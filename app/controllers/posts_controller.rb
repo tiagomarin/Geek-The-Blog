698231cfacf7b56d186b2742dbd94f607506d464
Tiago Marin <tiago.lelinski@gmail.com>
2022-11-09 11:58:39 -0300

    add form to add a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,23 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comments = @post.comments
   end
+
+  def new
+    @new_post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.author = current_user
+    if post.save
+      redirect_to user_path(current_user.id)
+    else
+      flash.now[:error] = 'Error: Post could not be saved. Please try again'
+      render :new
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "users#index"
   
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:create]
       resources :likes, only: [:create]
     end
