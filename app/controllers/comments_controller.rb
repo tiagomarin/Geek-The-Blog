27234bd25a504e6978d833ae6d1b6daf2cda4a20
Tiago Marin <tiago.lelinski@gmail.com>
2022-11-09 15:39:11 -0300

    add form to add a comment to a post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    @comment.author = current_user
+    @comment.post = @post
+    if @comment.save
+      redirect_to user_post_path(@user, @post)
+    else
+      flash.now[:error] = 'Error: comment could not be saved. Please try again'
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,16 @@
 class LikesController < ApplicationController
+  def create
+    like = Like.new(like_params)
+    like.author = current_user
+    if like.save
+      redirect_to user_post_path(current_user.id)
+    else
+      flash.now[:error] = 'Error: like could not be saved. Please try again'
+      render :new
+    end
+  end
+
+  def like_params
+    params.require(:like).permit(:author, :post)
+  end
 end
