3f9e3ee02518fcd9fe40690eb3e69bc3f160f0e0
Tiago Marin <tiago.lelinski@gmail.com>
2022-11-18 23:50:56 -0300

    fix linter errors

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::ApplicationController < ActionController::API
   respond_to :json
 
   private
-  
+
   def current_user
     @current_user ||= User.find_by(id: doorkeeper_token[:resource_owner_id])
   end
